6908f5c0a0b056ca1d0988e38afe4d9d32d657f4
Gilia Patterson <32884936+giliapatterson@users.noreply.github.com>
2023-09-15 23:54:01 +0000

    Speed up finding parent-offspring pairs

--- close_kin_slim/spatial_sim_data/make_spaghetti.py
+++ close_kin_slim/spatial_sim_data/make_spaghetti.py
@@ -49,9 +49,7 @@ if (method == "random"):
 if (method == "biased"):
     sample_rows, ss = spatially_biased(parents, n)
 
-
-# Make this faster, maybe with a dictionary
-pairs = list(itertools.combinations(sample_rows, 2))
+sample_parents = parents.iloc[sample_rows, :]
 
 # creating new Image object for spaghetti and sampling
 w, h = 500, 500
@@ -60,25 +58,27 @@ sampling = Image.new("1", (w, h))
 img1 = ImageDraw.Draw(spaghetti)
 img2 = ImageDraw.Draw(sampling)
 
-#Number of parent offspring pairs
-npairs = 0
-# Plot spaghetti and sampling
-for pair in pairs:
-    ind0_i = pair[0]
-    ind1_i = pair[1]
-    ind0 = parents.iloc[ind0_i, :]
-    ind1 = parents.iloc[ind1_i, :]
-    x0 = ind0['x']*w
-    y0 = ind0['y']*h
-    x1 = ind1['x']*w
-    y1 = ind1['y']*h
-    if(ind0['individual'] == ind1['parent1'] or ind1['individual'] == ind0['parent1']):
-        img1.line([(x0, y0), (x1, y1)], fill ="white", width = 0)
-        npairs += 1
-    img2.point([(x0, y0), (x1, y1)], fill = "white") 
+# Find individuals with a parent that is in the sample
+p_in_i = np.isin(sample_parents.loc[:,'parent1'], sample_parents.loc[:,'individual'])
+npairs = sum(p_in_i)
 
+# Get location of parent-offspring pairs and plot
+children = sample_parents.loc[p_in_i, :]
+for child_i, child_row in children.iterrows():
+    parent_i = np.where(sample_parents.loc[:,'individual'] == child_row['parent1'])[0][0]
+    parent_row = sample_parents.iloc[parent_i,:]
+    # print(parent_row['individual'], child_row['individual'])
+    x_child, y_child = child_row[['x','y']]
+    x_parent, y_parent = parent_row[['x','y']]
+    img1.line([(x_child*w, y_child*h), (x_parent*w, y_parent*h)], fill ="white", width = 0)
+
+# Plot sampling
+for ind_i, ind_row in sample_parents.iterrows():
+    x, y = ind_row[['x','y']]
+    img2.point((x*w, y*h), fill = "white")
 
 spaghetti.save(spaghetti_out)
 sampling.save(sampling_out)
+
 with open(outN, "w") as f:
     f.write(spaghetti_out.replace("_spaghetti.png", "") + "," + str(N) + "," + str(ss) + "," + str(npairs) + "\n")
