3f0fa375511cba1a8be8bbc23302745dc9bc65d4
Gilia Patterson <32884936+giliapatterson@users.noreply.github.com>
2023-11-08 14:07:29 -0800

    Implement bearded seal simulation from Conn paper

---
+++ replicate_conn/leslie_model.R
@@ -0,0 +1,31 @@
+C = 1.3
+ETA1 = 0.055
+ETA2 = 2.80
+ETA3 = 0.076
+
+ages = 0:60
+p_breed = 1/((1 + exp(-1.264*(ages-5.424))))
+survival = exp(-C*((ETA1*ages)^ETA2 + (ETA1*ages)^(1/ETA2) + ETA3*ages))
+p_mate = 1/((1+exp(-1.868*(ages - 6.5))))
+
+# Survival greater than about 0
+nz_surv <- survival > 10e-4
+survival <- survival[nz_surv]
+p_breed <- p_breed[nz_surv]
+p_mate <- p_mate[nz_surv]
+ages <- ages[nz_surv]
+
+plot(ages, survival)
+plot(ages, p_breed)
+plot(ages, p_mate)
+
+# Leslie matrix for females
+# Survival
+Surv = diag(survival)
+# First row is female breeding probability divided by 2
+Fec = matrix(p_breed/2, nrow = 1)
+# Leslie matrix
+M = rbind(Fec, Surv[1:(nrow(Surv)-1),])
+# Find eigenvalues
+eigen(M)
+
