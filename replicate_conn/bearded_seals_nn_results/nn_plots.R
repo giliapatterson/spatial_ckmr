71ef137717ed0d80945fc76c979e2ed94b392c55
Gilia Patterson <32884936+giliapatterson@users.noreply.github.com>
2024-06-11 17:35:17 -0700

    Record number of comparisons for POPs and HSPs

--- replicate_conn/bearded_seals_nn_results/nn_plots.R
+++ replicate_conn/bearded_seals_nn_results/nn_plots.R
@@ -12,8 +12,16 @@ ggplot(seals_seals, aes(x = truth, y = bearded_nn_pred, color = bias)) +
 
 ggplot(seals_seals, aes(x = truth, y = bearded_nn_pred)) +
   geom_point() +
-  ggtitle("Trained on: bearded seals\nTested on: bearded seals") +
+  ggtitle("Performance of CKMR network for different levels of spatial sampling bias") +
   xlab("True N") +
   ylab("Predicted N") +
   geom_abline(slope = 1) +
-  facet_wrap(~bias)
+  facet_wrap(~bias, labeller = "label_both")
+
+random_seals <- filter(seals_seals, bias == 1)
+ggplot(random_seals, aes(x = truth, y = bearded_nn_pred)) +
+  geom_point() +
+  ggtitle("Performance of CKMR network for different levels of spatial sampling bias") +
+  xlab("True N") +
+  ylab("Predicted N") +
+  geom_abline(slope = 1)
--- replicate_conn/seal_sim_figs.R
+++ replicate_conn/seal_sim_figs.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(mgcv)
+theme_set(theme_gray(base_size = 22))
+
 seal_parents <- read.csv("bearded_seal_parents.csv")
 ggplot(seal_parents, aes(x = age)) +
   geom_histogram(binwidth = 1)
@@ -39,3 +42,20 @@ ggplot(labels, aes(x = N_avg, y = n)) +
   ylab("Sample size") +
   facet_wrap(~bias)
 
+ggplot(labels, aes(x = N_avg, y = nsibs + npops)) +
+  geom_point() +
+  geom_smooth() +
+  xlab("Average N") +
+  ylab("Number of half-sibling and parent-offspring pairs") +
+  facet_wrap(~bias, labeller = "label_both") 
+
+labels_random <- mutate(labels, total_pairs = npops + nsibs) %>% filter(bias == 1)
+model <- gam(N_avg ~ s(total_pairs, bs = "cs"), data = labels_random)
+labels_random$fitted_N = model$fitted.values
+labels_random[which(between(labels_random$fitted_N, 11990, 12010)),]
+
+ggplot(labels_random, aes(x = total_pairs, y = N_avg)) +
+  geom_point() +
+  geom_line(aes(y = fitted_N), color = "blue") +
+  xlab("Average N") +
+  ylab("Number of half-sibling and parent-offspring pairs")
